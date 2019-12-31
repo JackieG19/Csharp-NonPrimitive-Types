9a0276b2334304abbd7a0826ef6434bdeeef93b5
Jackie <JGtheophila19@gmail.com>
2019-12-30 21:20:34 -0500

    the Program class will run the Person class  file

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,18 +1,7 @@
 using System;
 
 namespace CSharpFundamentals
-{
-	public class Person
-	{
-		public string FirstName;
-		public string LastName;
-		
-		public void Introduce()
-		{
-			Console.WriteLine("My name is " + FirstName + "" + LastName);
-		}
-	}
-	
+{	
 	class Program
 	{
 		static void Main(string[] args)
