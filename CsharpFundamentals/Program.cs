9ec54fa49c7781cf5c95aa49efe887039f9b1ac3
Jackie <JGtheophila19@gmail.com>
2019-12-30 21:19:00 -0500

    creating Person class

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/*using statement*/
+using System;
 
-namespace HelloWorld
+namespace CSharpFundamentals
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
-    }
+	public class Person
+	{
+		public string FirstName;
+		public string LastName;
+		
+		public void Introduce()
+		{
+			Console.WriteLine("My name is " + FirstName + "" + LastName);
+		}
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Person john = new Person();
+			var john = new Person();
+			john.FirstName = "John";
+			john.LastName = "Smith";
+			john.Introduce;
+		}	
+	}
 }
