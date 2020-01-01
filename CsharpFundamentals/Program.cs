46ea1dae1e963430e87363eee13a28f2aad30d6f
Jackie <JGtheophila19@gmail.com>
2020-01-01 14:54:09 -0500

    array with boolean

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -12,8 +12,6 @@ namespace CSharpFundamentals
 			Console.WriteLine(flags[0]);
 			Console.WriteLine(flags[1]);
 			Console.WriteLine(flags[2]);
-			
-			var names = new string[3] {"Jack", "John", "Mary"};
 		}	
 	}
 }
