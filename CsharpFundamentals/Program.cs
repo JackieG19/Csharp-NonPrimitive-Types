8f12e82b6dde37ed381bef85c3c987dc7c464791
Jackie <JGtheophila19@gmail.com>
2020-01-01 14:53:40 -0500

    array of strings

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -5,14 +5,7 @@ namespace CSharpFundamentals
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			var flags = new bool[3];
-			flag[0] = true;
-			
-			Console.WriteLine(flags[0]);
-			Console.WriteLine(flags[1]);
-			Console.WriteLine(flags[2]);
-			
+		{	
 			var names = new string[3] {"Jack", "John", "Mary"};
 		}	
 	}
