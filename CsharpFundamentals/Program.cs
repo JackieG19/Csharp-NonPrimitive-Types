4171152cb03994ae99ddbb90904e72e28ffe686d
Jackie <JGtheophila19@gmail.com>
2020-01-01 19:02:54 -0500

    using the string.Join method

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -6,15 +6,13 @@ namespace CSharpFundamentals
 	{
 		static void Main(string[] args)
 		{
-			var firstName = "Mosh";
-			var lastName = "Hamedani";
+			var names = new string[3] {"John", "Jack", "Mary"};
 			
-			var fullName = firstName + " " + lastName; // concatenate strings
-			
-			var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
-			
-			/*string.Format method - Converts the value of objects to strings based 
-			on the formats specified and inserts them into another string.*/
+			var formattedNames = string.Join(",", names);
+			/*string.Join method - The string.Join method combines many strings 
+			into one. It receives two arguments: an array and a separator string.*/
+
+			Console.WriteLine(formattedNames);
 		}	
 	}
 }
